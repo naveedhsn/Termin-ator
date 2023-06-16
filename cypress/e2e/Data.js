e1a60bfaa0b187b7c05fdbcfcacfa26bfacc6d25
naveedhsn <naveedhossaiin@gmail.com>
2023-06-16 19:12:12 +0600

    Data file

---
+++ cypress/e2e/Data.js
@@ -0,0 +1,32 @@
+// Data of add_company........................
+export const company_name = ["Apple", "Google", "Microsoft", "Tesla", "SpaceX", "Samsung",
+     "Redmi", "Vivo", "RealMe", "Oneplus", "Apple", "Google", "Microsoft", "Tesla", "SpaceX", "Samsung",
+     "Redmi", "Vivo", "RealMe", "Oneplus"]
+
+export const company_alias = ["Smith", "Robert", "Lukia", "Simi", "Donald", "Joe", "Ivanka", "Bulls", "Kuls", "John",
+     "Smith", "Robert", "Lukia", "Simi", "Donald", "Joe", "Ivanka", "Bulls", "Kuls", "John"]
+
+export const street = ["201 Main St.", "15 Maple St.", "104 Elm St.", "C20 Main St.", "Y15 Maple St.", "111 Elm St.", 
+     "XQ Main St.", "45 Maple St.", "PO Elm St.", "Link Road", "201 Main St.", "15 Maple St.", "104 Elm St.", "C20 Main St.", "Y15 Maple St.", "111 Elm St.", 
+     "XQ Main St.", "45 Maple St.", "PO Elm St.", "Link Road"]
+
+export const house = ["11", "22", "33", "44", "55", "66", "77", "88", "99", "100", "11", "22", "33", "44", 
+     "55", "66", "77", "88", "99", "100"]
+
+export const post_code = ["1000", "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008", "1009", "1010",
+     "1000", "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008", "1009", "1010"]
+
+export const ort = ["Zürich", "Geneva", "Basel", "Lausanne", "Bern", "Winterthur", "Lucerne", "Fribourg", "Thun", 
+     "Sankt Gallen", "Zürich", "Geneva", "Basel", "Lausanne", "Bern", "Winterthur", "Lucerne", "Fribourg", "Thun", 
+     "Sankt Gallen"]
+
+//Data of add_company_admin....................
+export const first_name = ["John", "Robert", "Donald", "Elon", "Joe", "Ivanka",
+"Juyena", "Merlin", "Lucia", "Hanni", "John", "Robert", "Donald", "Elon", "Joe", "Ivanka",
+"Juyena", "Merlin", "Lucia", "Hanni", "John", "Robert", "Donald", "Elon", "Joe", "Ivanka",
+"Juyena", "Merlin", "Lucia", "Hanni"]
+
+export const last_name = ["Doe", "Brown", "Trump", "Musk", "Biden", "Trump",
+"Beris", "Desouza", "Born", "Dus", "Doe", "Brown", "Trump", "Musk", "Biden", "Trump",
+"Beris", "Desouza", "Born", "Dus", "Doe", "Brown", "Trump", "Musk", "Biden", "Trump",
+"Beris", "Desouza", "Born", "Dus"]
